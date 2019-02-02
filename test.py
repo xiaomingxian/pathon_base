1db86fd1877c89fdd75c824c1b4f5e1c0a5535b4
xxm <934792834@qq.com>
2019-02-02 23:35:19 +0800

    说明

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-{"code": "0", "time": "1547608633722", "msg": "查询成功", "data": {
-    "districtSendResult": [{"districtName": "琼州", "districtSendCount": 55, "districtSuccessCount": 21},
-                           {"districtName": "秀英", "districtSendCount": 37, "districtSuccessCount": 18},
-                           {"districtName": "美兰", "districtSendCount": 50, "districtSuccessCount": 30},
-                           {"districtName": "龙华", "districtSendCount": 10, "districtSuccessCount": 10}],
-    "userSendResult": [{"sendName": "小张", "sendCount": 70}, {"sendName": "小李", "sendCount": 30},
-                       {"sendName": "小王", "sendCount": 20}, {"sendName": "小赵", "sendCount": 10}],
-    "totalSendResult": {"taskCount": 110, "userCount": 130, "messageCount": 180, "messageSuccCount": 90,
-                        "messageSuccRatio": 0.74}}, "count": null}
+# 知乎数据爬取
+# xpath-->//div[contains{@class,'i'}]
+# 贴吧爬虫02
