be610e77faf40707b9d7037615f8fff5738d8bb3
xxm <934792834@qq.com>
2019-06-02 21:49:25 +0800

    三元运算写法

--- test/assert_test.py
+++ test/assert_test.py
@@ -2,7 +2,7 @@ def main():
 
     assert 1/1
     assert 1/0
-
+    x=1
 
     pass
 
---
+++ test/三元运算.py
@@ -0,0 +1,8 @@
+def main():
+    x = "x" if 1 == 2 else "y"
+    print(x)
+    pass
+
+
+if __name__ == '__main__':
+    main()
