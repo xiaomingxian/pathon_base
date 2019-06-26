5854574d43eab76f499aa94b660edb1fedfd6c61
xxm <934792834@qq.com>
2019-06-27 00:00:56 +0800

    figure

--- 3.数据/code/0.matplotlib.py
+++ 3.数据/code/0.matplotlib.py
@@ -1,30 +1,16 @@
 import matplotlib
+
 # 独立窗口
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy
 
-x = [range(0, 20,2)]
-y = [range(10,30,2)]
-
-def base():
-    # 参数说明
-    # help(plt.plot)
-    # 设置图片大小[尺寸，清晰度]  figure就是窗口对象
-    plt.figure(figsize=(20,8),dpi=80)
-    # 绘图
-    plt.plot(x,y,color='green',linewidth=2.0)
-    # 展示图片
-    plt.show()
-    # 保存图片
-    plt.savefig('../result/0.matplotlib1.png')
-    pass
 
 def haveData():
     # -1~1生成50个随机数
-    xx=numpy.linspace(-1,1,50)
+    xx = numpy.linspace(-1, 1, 50)
     # 斜线公式
-    yy=2*xx+1
+    yy = 2 * xx + 1
     # 设置图片大小[尺寸，清晰度]  figure就是窗口对象
     plt.figure(figsize=(20, 10), dpi=80)
     # 绘图
@@ -40,20 +26,33 @@ def haveData():
     # 后调用了plt.savefig() ，在plt.show()
     # 后实际上已经创建了一个新的空白的图片（坐标轴），这时候你再plt.savefig()
     # 就会保存这个新生成的空白图片。
-
+    # 解决方式1：调整顺序
     # 解决方式2：画图的时候获取当前图像（这一点非常类似于Matlab的句柄的概念）：
-    #
     # # gcf: Get Current Figure
     # fig = plt.gcf()
     # plt.show()
     # fig.savefig('tessstttyyy.png', dpi=100)
 
-    pass
+def figure_use():
+    x=numpy.linspace(-1,1,50)
+    y1=x**2
+    y2=x**3
+    # 一个figure是一个窗口对象--按照顺序来[指定序号与长宽]
+    plt.figure(num=3,figsize=(8,10))
+    plt.plot(x,y1)
+    plt.figure()
+    # 同一个figure放入多个图像
+    plt.plot(x,y2)
+    # --表示虚线
+    plt.plot(x,y1,color='red',linewidth=5.0,linestyle='--')
+    plt.show()
+
+
 
 
 def main():
-    # base()
-    haveData()
+    # haveData()
+    figure_use()
     pass
 
 
