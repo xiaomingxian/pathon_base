20093851d14620cc0ea5c5d2cb54f3ef8b0d8c83
xxm <934792834@qq.com>
2019-07-27 23:26:29 +0800

    机器学习算法

---
+++ 6.机器学习/code/4.转换器与估计器.py
@@ -0,0 +1,23 @@
+from sklearn.preprocessing import StandardScaler
+
+
+# 转换器
+# fit_transform===> fit输入数据[计算出各种标准eg:平均数，中位数...] transform转换数据
+# 估计器
+# 估计器(estimator)是一类实现了算法的api
+# **************  1.用于分类的估计器： **************
+# sklearn.neighbors  k近邻算法
+# sklearn.naive_bayes 贝叶斯
+# sklearn.linear_model.LogisticRegression 逻辑回归
+# sklearn.tree  决策树与随机森林
+# **************  2.用于回归的估计器 **************
+# sklearn.linear_model.LinearRegression  线性回归
+# sklearn.linear_model.Ridge  岭回归
+# **************  2.聚类[略] **************
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
