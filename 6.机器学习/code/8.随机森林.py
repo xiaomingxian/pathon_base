fd94f039cd92d9a3d0987ab2d3eabb572bb95abe
xxm <934792834@qq.com>
2019-08-04 20:47:59 +0800

    机器学习-随机森林

--- 6.机器学习/code/8.随机森林.py
+++ 6.机器学习/code/8.随机森林.py
@@ -1,17 +1,12 @@
+# ensemble集成学习
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-# 导入字典
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.model_selection import train_test_split
-# 决策树
-from sklearn.tree import DecisionTreeClassifier, export_graphviz  # 可视化
-import graphviz
+# GridSearchCV 交叉验证
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 
 def main():
-    '''
-    泰坦尼克号预测生死
-    :return:
-    '''
     data = pd.read_csv("http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt")
     # print(data)
     # 提取特征 特征值与目标值
@@ -33,33 +28,22 @@ def main():
     x_train = dic.fit_transform(x_train.to_dict(orient='records'))  # orient='records') 一行对应一条记录
     x_test = dic.fit_transform(x_test.to_dict(orient='records'))  # orient='records') 一行对应一条记录
 
-    print(dic.get_feature_names())
-    # print(x_train)
-    # print(x_test)
+    # print(dic.get_feature_names())
 
-    # 使用决策树[估计器]
-    dec = DecisionTreeClassifier(max_depth=5)
-    dec.fit(x_train, y_train)
+    # 随机森林进行预测(超参数调优)
+    rf = RandomForestClassifier()
 
-    print('预测结果-准确率：', dec.score(x_test, y_test))
+    # 决策树数量  树深度  6*5=30次的组合
+    param = {'n_estimators': [120, 200, 300, 500, 800, 1200], 'max_depth': [5, 8, 15, 25, 30]}
 
-    # 导出决策树的结构
-    export_graphviz(dec, out_file='../pic/shu.dot', feature_names=['年龄', '一层', '二层', '三层', '女性', '男性'])
+    # 此处 cv=2 每个参数有两次交叉验证
+    gc = GridSearchCV(rf, param_grid=param, cv=2)
+    gc.fit(x_train, y_train)
 
-    pass
-
-
-def read_dot():
-    # 环境未配置
-    # pip3 install graphviz
-    with open('../pic/shu.dot') as f:
-        fr = f.read()
-    dot = graphviz.Source(fr)
-    dot.view()
-
+    print('准确率', gc.score(x_test, y_test))
+    print('best:', gc.best_params_)
     pass
 
 
 if __name__ == '__main__':
     main()
-    # read_dot()
